4ff301ecdab53027b795280f39db717845976ce1
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-11-16 17:24:35 -0300

    Feat: ok

--- src/controllers/animais.controller.js
+++ src/controllers/animais.controller.js
@@ -3,6 +3,10 @@ import { AnimalsLista} from "../models/animal/animalLista.js"
 
 const animallista = new AnimalsLista();
 
+const isURL = (url) =>{
+    return url.match(/\.(jpeg|jpg|png|gif)$/) != null;
+}
+
 export const getAnimals = (req, res) =>{
     const animals = animallista.getAnimals();
 
@@ -10,6 +14,7 @@ export const getAnimals = (req, res) =>{
         return res.status(200).json(animals);
     }
     return res.status(200).json({message: "Não há animais cadastrados"});
+
 }
 
 export const getAnimal = (req, res) => {
@@ -23,11 +28,40 @@ export const getAnimal = (req, res) => {
 
   export const createAnimal = (req, res) => {
     const { name, type, age, color, image, vaccine } = req.body;
+
+    if(!name || !type || !age || !color || !image ){
+        return res.status(400).send({Message:"Dados inválidos"})
+    }
+
+    if(name.length < 3 || name.length > 50){
+        return res.status(400).send({message:"Nome inválido"})
+    }
+    if(age < 0 ){
+        return res.status(400).send({message:"Idade inválida"})
+    }
+
+    if(type.length > 30){
+        return res.status(400).send({message: "Tipo inválido"})
+    }
+
+    if(color.length > 20){
+        return res.status(400).send({message: "Cor inválida"})
+    }
+    if(typeof(vaccine) != 'boolean'){
+        return res.status(400).send({message:"Responda com true ou false"})
+    }
+    if(!(isURL(image))){
+        return res.status(400).send({message:"Imagem inválida"})
+    }
+
+    
     const animal = new Animal(name, type, age, color, image, vaccine);
 
     animallista.addAnimal(animal);
 
     return res.status(201).send(animal);
+
+    
   }
 
   export const updateAnimal = (req, res) =>{
